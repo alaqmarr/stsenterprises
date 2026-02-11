013f8374bdb1eafad395f871e70b41a61a2217d3
Al Aqmar <alaqmarak0810@gmail.com>
2026-02-11 11:29:51 +0530

    deploy

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,12 @@
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 export const prisma = new PrismaClient({ adapter });
 const db = prisma;
 export default db;
